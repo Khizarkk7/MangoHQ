fe2b30a7f8a693573108be5f7943600d014af847
Yasiralishah21 <155631266+Yasiralishah21@users.noreply.github.com>
2025-06-23 20:10:45 +0500

    "Navbar&HeroSec done"

---
+++ mango-app/src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+export default function shop(){
+    return(
+        <div className="p-4 ">
+            <h1>what are you looking for?</h1> <br />
+            <label htmlFor="">search anything here ...</label> <br />
+            <input type="text" className="border border-black" placeholder="search" />
+        </div>
+    )
+}
---
+++ mango-app/src/app/wishlist/page.tsx
@@ -0,0 +1,8 @@
+
+export default function wishlist(){
+    return(
+        <div>
+            <h1>YOUR WISHLIST</h1>
+        </div>
+    )
+}
